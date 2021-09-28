ee7549515fc9947741573c1e380f54b237ca4558
gytj2013 <63896397+gytj2013@users.noreply.github.com>
2021-09-29 00:11:44 +0900

    프로그래머스 lv2 오픈 채팅방 - 완료

---
+++ src/programmers/level02/OpenChatRoom/OpenChatRoom_gytj2013.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+class Solution {
+    public static String[] solution(String[] record) {
+        String[] answer = {};
+        HashMap<String, String> usermap = new HashMap<String, String>();
+        int cnt = 0;
+        
+        for(int i=0; i<record.length; i++) {
+        	String[] tmp = record[i].split(" ");
+        	switch(tmp[0]) {
+        	case "Leave" :
+        		cnt ++;
+        		break;
+        	case "Enter" :
+        		cnt++;
+        	default : //case "Enter" & "Change"
+        		usermap.put(tmp[1], tmp[2]);
+        		break;
+        	}
+        }
+        
+        answer = new String[cnt];
+        cnt = 0;
+        
+        for(int i=0; i<record.length; i++) {
+        	String[] tmp = record[i].split(" ");
+        	switch(tmp[0]) {
+        	case "Enter" :
+        		answer[cnt] = usermap.get(tmp[1])+"님이 들어왔습니다.";
+        		cnt++;
+        		break;
+        	case "Leave" :
+        		answer[cnt] = usermap.get(tmp[1])+"님이 나갔습니다.";
+        		cnt++;
+        		break;
+        	default : //case "Change"
+        		break;
+        	}
+        }
+        return answer;
+    }
+}
