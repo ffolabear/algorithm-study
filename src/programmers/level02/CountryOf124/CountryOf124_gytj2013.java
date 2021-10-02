3d4f3b7233e2eb0a100c5b80821395ba73e8f1fb
gytj2013 <63896397+gytj2013@users.noreply.github.com>
2021-10-02 20:20:31 +0900

    프로그래머스 lv2 124 나라의 숫자 - 풀었음

---
+++ src/programmers/level02/CountryOf124/CountryOf124_gytj2013.java
@@ -0,0 +1,14 @@
+package programmers.level02.CountryOf124;
+
+class CountryOf124_gytj2013 {
+    public static String solution(int n) {
+    	String answer = "";
+        String[] arr = {"4", "1", "2"};
+       
+        while(n > 0) {
+        	answer = arr[n%3] + answer;
+        	n = (n-1)/3;
+        }
+        return answer;
+    }
+}
