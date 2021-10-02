1c957b62a23ab04fd08a3e316ff0205aa0859864
gytj2013 <63896397+gytj2013@users.noreply.github.com>
2021-10-02 16:51:29 +0900

    프로그래머스 lv2 카카오프렌즈 멀쩡한 사각형 - 풀었음

---
+++ src/programmers/level02/NormalSquare/NormalSquare_gytj2013.java
@@ -0,0 +1,16 @@
+package programmers.level02.NormalSquare;
+
+class NormalSquare_gytj2013 {
+  public long solution(long w, long h) {
+        long big = w>h ? w:h;
+        long small = w>h ? h:w;
+        
+        while(small!=0) {
+	        long tmp = small;
+	        small = big % small;
+	        big = tmp;
+        }
+        
+        return w*h-(w+h-big);
+    }
+}
