8971c29af46b7eca70558748cea1fbce4c7ca98b
gytj2013 <63896397+gytj2013@users.noreply.github.com>
2021-10-04 21:34:02 +0900

    프로그래머스 lv2 기능 개발 - 풀었음

---
+++ src/programmers/level02/FunctionDevelopment/FunctionDevelopment_gytj2013.java
@@ -0,0 +1,31 @@
+package programmers.level02.FunctionDevelopment;
+
+import java.util.ArrayList;
+
+class FunctionDevelopment_gytj2013 {
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] dayleft = new int[progresses.length];
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        
+        for(int i =0; i<progresses.length;i++) {
+        	int tmp = 100 - progresses[i];
+        	dayleft[i] = tmp / speeds[i];
+        	if(tmp%speeds[i]!=0) {dayleft[i]++;}
+        }
+        
+        int cnt=1;
+        int tmp = dayleft[0];
+        
+        for(int i =1; i<progresses.length;i++) {
+        	if(dayleft[i] <= tmp) {cnt++;}
+        	else {
+        		answer.add(cnt);
+        		cnt = 1;
+        		tmp = dayleft[i];
+        	}
+        }
+        answer.add(cnt);
+       
+        return  answer.stream().mapToInt(i->i).toArray();
+    }
+}
