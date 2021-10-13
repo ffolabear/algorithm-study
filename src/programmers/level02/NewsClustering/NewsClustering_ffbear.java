f2c7c6eae17ea0222d0c7941a9a0f4cd68c2b354
ffbear <kth6699@naver.com>
2021-10-14 00:31:12 +0900

    프로그래머스 lv2 [1차] 뉴스 클러스터링 - 풀었음

--- src/programmers/level02/NewsClustering/NewsClustering_ffbear.java
+++ src/programmers/level02/NewsClustering/NewsClustering_ffbear.java
@@ -1,8 +1,6 @@
 package programmers.level02.NewsClustering;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class NewsClustering_ffbear {
     public int solution(String str1, String str2) {
@@ -13,21 +11,13 @@ public class NewsClustering_ffbear {
         ArrayList<String> str1Arr = new ArrayList<>();
         ArrayList<String> str2Arr = new ArrayList<>();
 
-        String regex = "[a-z]";
-
-
-        System.out.println(str1);
-        System.out.println(str2);
-
-//        ArrayList<String> union = new ArrayList<>();
-        Map<String, Integer> union = new HashMap<>();
+        ArrayList<String> union = new ArrayList<>();
 
         for (int i = 0; i < str1.length() - 1; i++) {
             char a = str1.charAt(i);
             char b = str1.charAt(i + 1);
 
             if (a >= 'a' && a <= 'z' && b >= 'a' && b <= 'z') {
-                union.put(String.valueOf(a) + b, 0);
                 str1Arr.add(String.valueOf(a) + b);
             }
 
@@ -38,7 +28,6 @@ public class NewsClustering_ffbear {
             char b = str2.charAt(i + 1);
 
             if (a >= 'a' && a <= 'z' && b >= 'a' && b <= 'z') {
-                union.put(String.valueOf(a) + b, 0);
                 str2Arr.add(String.valueOf(a) + b);
             }
 
@@ -47,32 +36,25 @@ public class NewsClustering_ffbear {
 
         int intersection = 0;
 
-        for (int i = 0; i < str1Arr.size(); i++) {
-            for (int j = 0; j < str2Arr.size(); j++) {
-                if (str1Arr.get(i).equals(str2Arr.get(j))) {
-                    intersection++;
-                }
+        for (String s : str1Arr) {
+            if (str2Arr.remove(s)) {
+                intersection++;
             }
+            union.add(s);
+        }
+
+        union.addAll(str2Arr);
+        int answer = 0;
+
+        if (union.size() == 0) {
+            answer = 65536;
+        } else {
+            double j = (double) intersection / (double) union.size() * 65536;
+            answer = (int) Math.floor(j);
 
         }
 
-        if (intersection > str1Arr.size() || intersection > str2Arr.size()) {
-            intersection = Math.min(str1Arr.size(), str2Arr.size());
-        }
 
-        System.out.println("str1 : " + str1Arr);
-        System.out.println("str2 : " + str2Arr);
-        System.out.println("합집합 : " + union + " / " + union.size());
-        System.out.println("교집합의 수 : " + intersection);
-
-
-        double f = (float) intersection / union.size() * 65536;
-        int answer = (int) Math.floor(f);
-
-        System.out.println(answer);
-
-
-        System.out.println();
         return answer;
     }
 
