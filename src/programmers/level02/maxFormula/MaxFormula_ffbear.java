de929d8bebde3d169a9fcb2a8e78e84e71b412f7
ffbear <kth6699@naver.com>
2021-10-16 06:04:13 +0900

    프로그래머스 lv2 수식 최대화 - 완료

--- src/programmers/level02/maxFormula/MaxFormula_ffbear.java
+++ src/programmers/level02/maxFormula/MaxFormula_ffbear.java
@@ -4,50 +4,88 @@ import java.util.ArrayList;
 
 public class MaxFormula_ffbear {
 
-    static ArrayList<String> list;
-    static long[] nums;
+    //연산자 배열
+    static ArrayList<Character> oper;
+    //숫자 배열
+    static ArrayList<Long> nums;
+
+    static char[] prior = {'+', '-', '*'};
+    static boolean[] check = new boolean[3];
+    static long answer;
 
     public long solution(String expression) {
 
         String temp = "";
+        answer = 0;
 
         String regex = "[^0-9]";
         temp = expression.replaceAll(regex, " ");
-        list = new ArrayList<>();
+        oper = new ArrayList<Character>();
 
         for (int i = 0; i < expression.length(); i++) {
             if (String.valueOf(expression.charAt(i)).matches(regex)) {
-                list.add(String.valueOf(expression.charAt(i)));
+                oper.add(expression.charAt(i));
             }
         }
 
         String[] numsStr = temp.split(" ");
-        nums = new long[numsStr.length];
+        nums = new ArrayList<>();
 
         for (int i = 0; i < numsStr.length; i++) {
-            nums[i] = Long.parseLong(numsStr[i]);
-            System.out.println(nums[i]);
+            nums.add(Long.parseLong(numsStr[i]));
         }
 
 
+        dfs(0, new char[3]);
 
-
-
-        long answer = 0;
+        System.out.println(answer);
         return answer;
     }
 
+    static void dfs(int count, char[] order) {
 
 
-    static Long calculation(Long num1, Long num2, String operator) {
+        if (count == 3) {
 
+            ArrayList<Long> numsClone = (ArrayList<Long>) nums.clone();
+            ArrayList<Character> operClone = (ArrayList<Character>) oper.clone();
+
+            for (int i = 0; i < order.length; i++) {
+                for (int j = 0; j < operClone.size(); j++) {
+
+                    if (order[i] == operClone.get(j)) {
+                        Long res = calculation(numsClone.remove(j), numsClone.remove(j), order[i]);
+                        numsClone.add(j, res);
+                        operClone.remove(j);
+                        j--;
+                    }
+                }
+            }
+            answer = Math.max(answer, Math.abs(numsClone.get(0)));
+            return;
+        }
+
+
+        // 모든 우선순위 조합구하기.
+        for (int i = 0; i < 3; i++) {
+            if (!check[i]) {
+                check[i] = true;
+                order[count] = prior[i];
+                dfs(count + 1, order);
+                check[i] = false;
+            }
+        }
+
+    }
+
+    static Long calculation(long num1, long num2, char operator) {
         long res = 0;
-        if (operator.equals("-")) {
-            res = num1 - num2;
-        } else if (operator.equals("*")) {
-            res = num1 * num1;
-        } else if (operator.equals("+")) {
-            res = num1 + num2;
+        if (operator == '-') {
+            return num1 - num2;
+        } else if (operator == '*') {
+            return num1 * num2;
+        } else if (operator == '+') {
+            return num1 + num2;
         }
 
         return res;
