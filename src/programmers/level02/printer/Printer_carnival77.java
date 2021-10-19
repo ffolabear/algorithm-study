d927b6f26ccd050f6e98e04f1a86535d665c4bf7
carnival77 <stam0325@gmail.com>
2021-10-19 22:37:19 +0900

    프로그래머스 lv2 프린터 - 풀었음

---
+++ src/programmers/level02/printer/Printer_carnival77.java
@@ -0,0 +1,57 @@
+package programmers.level02.printer;
+import programmers.level02.phoneNumList.PhoneNumList_carnival77;
+
+import java.awt.*;
+import java.util.*;
+public class Printer_carnival77 {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        int print=0;
+
+        Queue<Point> queue = new LinkedList<Point>();
+
+        for(int i=0;i<priorities.length;i++){
+            queue.offer(new Point(priorities[i],i));
+        }
+
+        while(queue.size()>0){
+            Point tg = new Point();
+            tg = queue.poll();
+            int max_pr = 0;
+            if(queue.size()>0){
+                for(Point p : queue){
+                    max_pr = Math.max(max_pr,p.x);
+                }
+                if(tg.x < max_pr){
+                    queue.offer(tg);
+                }
+                else{
+                    print = print+1;
+                    if(location == tg.y){
+                        answer=print;
+                        return answer;
+                    }
+                }
+            }
+            else{
+                print = print+1;
+                if(location == tg.y){
+                    answer=print;
+                    return answer;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] priorities = {2, 1, 3, 2};
+        int location = 2;
+
+        Printer_carnival77 sol = new Printer_carnival77();
+        System.out.println(sol.solution(priorities,location));
+    }
+}
