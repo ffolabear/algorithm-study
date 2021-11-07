1b3993effef997855515772749b74457743a2454
jola7373 <jola7373@naver.com>
2021-11-07 13:47:46 +0900

    프로그래머스 lv2 피로 - 풀었음

---
+++ src/programmers/level02/fatigue/Fatigue_jola7373.java
@@ -0,0 +1,25 @@
+package programmers.level02.fatigue;
+
+public class Fatigue_jola7373 {
+    class Solution {
+        static boolean [] isVisit;
+        static int max = 0;
+        public static int solution(int k, int[][] dungeons) {
+            isVisit = new boolean[dungeons.length];
+            dungeonCount(dungeons, k, 1);
+            return max;
+        }
+        public static void dungeonCount(int[][] dungeons, int k, int depth){
+            for(int i=0; i < dungeons.length; i++){
+                if(!isVisit[i]){
+                    isVisit[i] = true;
+                    if(k >= dungeons[i][0]){
+                        max = Math.max(max, depth);
+                        dungeonCount(dungeons, k-dungeons[i][1],depth+1);
+                    }
+                    isVisit[i] = false;
+                }
+            }
+        }
+    }
+}
