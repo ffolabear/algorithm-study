db8bb47636d4194b25c01eb54827afd52ef5271e
carnival77 <stam0325@gmail.com>
2021-11-08 19:38:25 +0900

    프로그래머스 lv1 다트 게임 - 풀었음

--- src/programmers/level02/fatigue/Fatigue_jola7373.java
+++ src/programmers/level02/fatigue/Fatigue_jola7373.java
@@ -5,12 +5,12 @@ public class Fatigue_jola7373 {
     static int max = 0;
     class Solution {
 
-        public static int solution(int k, int[][] dungeons) {
+        public int solution(int k, int[][] dungeons) {
             isVisit = new boolean[dungeons.length];
             dungeonCount(dungeons, k, 1);
             return max;
         }
-        public static void dungeonCount(int[][] dungeons, int k, int depth){
+        public void dungeonCount(int[][] dungeons, int k, int depth){
             for(int i=0; i < dungeons.length; i++){
                 if(!isVisit[i]){
                     isVisit[i] = true;
